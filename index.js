5b90334355f604cf5db1ab87969f4d737cebcaaf
Francesco Montanaro <56433128+FrancescoMontanaro@users.noreply.github.com>
2022-04-04 14:25:37 +0200

    new home page

---
+++ index.js
@@ -0,0 +1,24 @@
+function countDown(){
+    let countDownDate = new Date(Date.UTC(2022, 4, 16, (14 - 2), 0, 0)).getTime();
+            
+    let x = setInterval(function() {
+        let now = new Date().getTime();
+        let distance = countDownDate - now;
+
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("countdown").innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("countdown").style.display = "none";
+            document.getElementById("buyNow").style.display = "block";
+        }
+    }, 1000);
+}
+
+
+countDown();
